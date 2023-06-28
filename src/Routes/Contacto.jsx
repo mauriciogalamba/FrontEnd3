5a0af4a1d13e0c7310a3bb3300c89ad8ac977284
Mauricio Galamba <mauricio@inetargentina.com>
2023-06-28 16:18:53 -0300

    corregido el formulario

--- src/Routes/Contacto.jsx
+++ src/Routes/Contacto.jsx
@@ -1,66 +1,76 @@
-import { useState } from "react"
-import styles from './Contacto.module.css'
+import { useState } from "react";
+import styles from "./Contacto.module.css";
 
 function Contacto() {
-  const [error, setError] = useState(false)
-  const [nombre, setNombre] = useState('')
-  const [msj, setMsj] = useState('')
+  const [error, setError] = useState(false);
+  const [nombre, setNombre] = useState("");
+  const [msj, setMsj] = useState("");
 
   function handleSubmit(e) {
-    e.preventDefault()
-    const name = e.target.nombre.value.trim()
-    const mail = e.target.email.value.trim()
-    
-    const [usuario, dominio] = mail.split('@')
-    const reg =/^[a-z0-9]{2,61}\.([a-z0-9]{2,61}|[a-z0-9-]{2,30}\.[a-z]{2,})$/gi
-    
-    if(name.length <5 ){
-      setMsj("El nombre debe contener al menos 5 caracteres.")
-      setError(true)
-      return
+    e.preventDefault();
+    const name = e.target.nombre.value.trim();
+    const mail = e.target.email.value.trim();
+
+    /* Validaciones adicionales al Required*/
+    const dominio = mail.split("@")[1];
+    const reg =
+      /^[a-z0-9]{2,61}\.([a-z0-9]{2,61}|[a-z0-9-]{2,30}\.[a-z]{2,})$/gi;
+
+    if (name.length < 5) {
+      setMsj("El nombre debe contener al menos 5 caracteres.");
+      setError(true);
+      return;
     }
 
-    if(!reg.test(dominio)){
-      setMsj("El email tiene dominio no válido.")
-      setError(true)
-      return
+    if (!reg.test(dominio)) {
+      setMsj("El email tiene dominio no válido.");
+      setError(true);
+      return;
     }
 
-    setNombre(name)
-    setError(false)
+    setNombre(name);
+    setError(false);
   }
- 
-  function handelChange(){
-    setNombre('')
-    setError(false)
+
+  function handelChange() {
+    /* Para quitar los mensajes si se modifica alguno de los input*/
+    setNombre("");
+    setError(false);
   }
-  
 
+  return (
+    <div className={styles.container}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <label htmlFor="nombre">Nombre: </label>
+        <input
+          type="text"
+          required
+          id="nombre"
+          placeholder="Ingresa tu nombre"
+          onChange={handelChange}
+        />
 
- return(
-  <div className={styles.container}>
-    <form className={styles.form} onSubmit={handleSubmit}>
-      <label htmlFor="nombre">Nombre: </label>
-      <input type="text" required
-      id="nombre"
-      placeholder="Ingresa tu nombre"
-      onChange={handelChange} 
-      />
-      <label htmlFor="email">Email: </label>
-      <input type="email" required
-      id="email"
-      placeholder="Ingresa tu email"
-      onChange={handelChange} 
-      />
-      <button type="submit">Enviar</button>
-    </form>
+        <label htmlFor="email">Email: </label>
+        <input
+          type="email"
+          required
+          id="email"
+          placeholder="Ingresa tu email"
+          onChange={handelChange}
+        />
 
-    {error&&
-      <div className={styles.error}>ERROR: {msj}</div>}
-    {nombre&&
-      <div>Gracias <i>{nombre}</i> nos pondremos en contato a la brevedad!</div>}
-  </div>
-  )
+        <button type="submit">Enviar</button>
+      </form>
+
+      {error && <div className={styles.error}>ERROR: {msj}</div>}
+      {nombre && (
+        <div>
+          Gracias <i>{nombre}</i>
+          nos pondremos en contato a la brevedad!
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Contacto;
